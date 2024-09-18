514e380a40fc19519743ebeca7fb3ca7d44d46e0
beanbag44 <pidgeonmaster64@gmail.com>
2024-09-19 00:48:58 +0100

    - swapped to Class instead of KClass

--- src/main/kotlin/me/beanbag/nuker/eventsystem/EventBus.kt
+++ src/main/kotlin/me/beanbag/nuker/eventsystem/EventBus.kt
@@ -2,17 +2,16 @@ package me.beanbag.nuker.eventsystem
 
 import me.beanbag.nuker.eventsystem.events.Event
 import me.beanbag.nuker.eventsystem.events.ICancellable
-import kotlin.reflect.KClass
 
 object EventBus {
-    val callbackHolderMap = hashMapOf<KClass<out Event>, ArrayList<Pair<KClass<*>, (Event) -> Unit>>>()
+    val callbackHolderMap = hashMapOf<Class<out Event>, ArrayList<Pair<Class<*>, (Event) -> Unit>>>()
 
-    inline fun <reified T: Event> subscribe(callbackHolder: KClass<*>, noinline callback: (Event) -> Unit) {
-        callbackHolderMap.getOrPut(T::class) { arrayListOf() }
+    inline fun <reified T: Event> subscribe(callbackHolder: Class<*>, noinline callback: (Event) -> Unit) {
+        callbackHolderMap.getOrPut(T::class.java) { arrayListOf() }
             .add(Pair(callbackHolder, callback))
     }
 
-    fun unsubscribe(clazz: KClass<*>) {
+    fun unsubscribe(clazz: Class<*>) {
         callbackHolderMap.values.forEach {
             it.removeIf { callbackPair ->
                 callbackPair.first == clazz
@@ -21,7 +20,7 @@ object EventBus {
     }
 
     fun post(event: Event) {
-        callbackHolderMap[event::class]?.let { callbackList ->
+        callbackHolderMap[event::class.java]?.let { callbackList ->
             for (pair in callbackList) {
                 pair.second.invoke(event)
                 if (event is ICancellable && event.isCanceled()) break
