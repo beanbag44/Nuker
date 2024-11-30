9be952817a1edf9d284161d10a1abb3bf2db59eb
pothemagicdragon <pothemagicdragon1@gmail.com>
2024-11-30 14:21:27 -0700

    format

--- src/main/kotlin/me/beanbag/nuker/ModConfigs.kt
+++ src/main/kotlin/me/beanbag/nuker/ModConfigs.kt
@@ -38,22 +38,21 @@ object ModConfigs {
         SetModuleListSettingCommand(),
     )
 
-    var modules =
-        listOf(
-            CoreConfig,
-            Nuker,
-            SourceRemover(),
-            UnfocusedCPU(),
-            FastBreak(),
-            EquipmentSaver(),
-            SafeWalk(),
-        )
+    var modules = listOf(
+        CoreConfig,
+        Nuker,
+        SourceRemover(),
+        UnfocusedCPU(),
+        FastBreak(),
+        EquipmentSaver(),
+        SafeWalk(),
+    )
 
     fun getModuleByName(name: String): Module? {
         return modules.find { it.name.equals(name, true) }
     }
 
-    fun <T:Module>getModuleByClass(clazz: Class<out T>): T? {
+    fun <T : Module> getModuleByClass(clazz: Class<out T>): T? {
         @Suppress("UNCHECKED_CAST")
         return modules.find { it.javaClass == clazz } as T?
     }
